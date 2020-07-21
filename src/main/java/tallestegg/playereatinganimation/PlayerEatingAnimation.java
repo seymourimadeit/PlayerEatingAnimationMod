403ba47b725192299a2e77f47e1c355bfcfef3f9
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-07-20 22:01:06 -0700

    eating animation for right hand

--- src/main/java/tallestegg/playereatinganimation/PlayerEatingAnimation.java
+++ src/main/java/tallestegg/playereatinganimation/PlayerEatingAnimation.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package tallestegg.playereatinganimation;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -14,16 +14,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(PlayerEatingAnimation.MODID)
+public class PlayerEatingAnimation
 {
     // Directly reference a log4j logger.
 
-    public static final String MODID = "examplemod";
+    public static final String MODID = "playereatinganimation";
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public PlayerEatingAnimation() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the doClientStuff method for modloading
---
+++ src/main/java/tallestegg/playereatinganimation/mixin/PlayerModelMixin.java
@@ -0,0 +1,36 @@
+package tallestegg.playereatinganimation.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+import net.minecraft.util.HandSide;
+import net.minecraft.util.math.MathHelper;
+
+@Mixin(PlayerModel.class)
+public class PlayerModelMixin <T extends LivingEntity> extends BipedModel<T>
+{	
+	public PlayerModelMixin(float p_i1148_1_) {
+		super(p_i1148_1_);
+	}
+
+	@Inject(at = @At("TAIL"), method = "setRotationAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V")
+	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo info) 
+	{
+	  ItemStack itemstack = entityIn.getHeldItemMainhand();
+	  if (itemstack.getUseAction() == UseAction.EAT && entityIn.isHandActive()) {
+          if (entityIn.getPrimaryHand() == HandSide.RIGHT) 
+          {
+            this.bipedRightArm.rotateAngleY = -0.5F;
+            this.bipedRightArm.rotateAngleX = -1.3F;
+            this.bipedRightArm.rotateAngleZ =  MathHelper.cos(ageInTicks) * 0.05F;
+          }
+	  }
+	}
+}
